af4633491775bf9b84157e079983085862ac556f
Baolong <baolong4102k@gmail.com>
2021-07-05 14:42:25 +0700

    ok

--- app/src/main/java/com/example/appdoctruyen/ManThongTinApp.java
+++ app/src/main/java/com/example/appdoctruyen/ManThongTinApp.java
@@ -15,9 +15,14 @@ public class ManThongTinApp extends AppCompatActivity {
 
         txtThongTin = findViewById(R.id.textviewthongtin);
 
-        String thongtin = " Ứng dụng được tạo ra bởi Long \n" +
-                " Đẹp trai\n" +
-                " Vô cùng luôn ";
+        String thongtin =
+                "Chào mừng bạn đến với ứng dụng đọc truyện android của bọn mình"+
+                " Ứng dụng được tạo ra bởi Nhóm 1: \n" +
+                "- Nguyễn Duy Bảo Long\n" +
+                "- Đỗ Trung Đức \n"+
+                "- Huỳnh Hải Công Huy \n"+
+                "- Nguyễn Văn Tiến \n"+"\n"+
+                "Ứng dụng làm ra với mục đích tìm hiểu về Android Studio, cho nên còn nhiều thiếu sót, hy vọng sẽ được mọi người đóng góp í kiến để nhóm phát triển thêm tính năng";
         txtThongTin.setText(thongtin);
     }
 }
